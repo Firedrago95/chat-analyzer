363dfe2043e37e3897387b05e053661e7611dd06
Firedrago95 <yonghwa95@naver.com>
2025-12-06 11:39:05 +0900

    chore: Chzzk Data Pipeline 초기 Spring Boot 프로젝트 환경 설정
    - Spring Boot 3.5.8 기반 프로젝트 생성 및 기본 구성
    - 데이터베이스 연동을 위한 MongoDB 의존성 추가
    - 웹 통신 및 실시간 처리를 위한 Web, WebSocket 의존성 추가
    - Gradle Wrapper 및 빌드 스크립트 설정 보완
    - Gitignore 파일 및 애플리케이션 기본 이름 설정

---
+++ src/main/java/io/github/hypecycle/chzzk/ChzzkDataPipelineApplication.java
@@ -0,0 +1,13 @@
+package io.github.hypecycle.chzzk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChzzkDataPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChzzkDataPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/hypecycle/chzzk/ChzzkDataPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hypecycle.chzzk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChzzkDataPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
